26e22881448494a0ed66f95c71b1fa6da12a4ee8
jielu <radiancelu@gmail.com>
2018-03-15 14:09:09 -0700

    import environment

--- src/app/services/widget.service.client.ts
+++ src/app/services/widget.service.client.ts
@@ -2,7 +2,8 @@ import {Injectable} from '@angular/core';
 import {Widget} from '../models/widget.model.client';
 import {Http, Response} from '@angular/http';
 import 'rxjs/Rx';
-import {environment} from '../../environments/environment';
+// import {environment} from '../../environments/environment';
+import {environment} from '../../environments/environment.prod';
 
 @Injectable()
 export class WidgetService {
