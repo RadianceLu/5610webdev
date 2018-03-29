5454492b579d84e8e5603a39ee3c434f5ec38886
jielu <radiancelu@gmail.com>
2018-03-28 20:44:18 -0700

    mongodb heroku try

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 //mongodb://<dbuser>:<dbpassword>@ds263847.mlab.com:63847/heroku_5b7815xd
 // var db = mongoose.connect('mongodb://localhost:27017/webdev');
-var db = mongoose.connect('mongodb://jielu:jielumangodb1@ds263847.mlab.com:63847/heroku_5b7815xd');
+var db = mongoose.connect('mongodb://heroku_5b7815xd:jielumangodb1@ds263847.mlab.com:63847/heroku_5b7815xd');
 
 module.exports = db;
